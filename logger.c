8c1017d67220d12edb6d52ee5923744d153e2b66
Riyan Maknojia <riyanmak123@gmail.com>
2026-03-11 18:38:15 -0500

    Minor fixes for logger program, everything works logger code is done

--- logger.c
+++ logger.c
@@ -18,16 +18,15 @@ void logger(char *logfileName) {
     
     }
     //Lets get the first non-whitespace characters
-    char *input = (char*)malloc(100 * sizeof(char));
-    
+    char *input = (char*)malloc(350 * sizeof(char));
+
     //if malloc fails exit the program rather than crashing
     if(input == NULL){
         perror("Malloc failed");
         exit(EXIT_FAILURE);
     }
 
-
-    char exit[] = "EXIT";
+    char quit[] = "QUIT";
     char Action[100];
     char Message[250];
 
@@ -37,19 +36,18 @@ void logger(char *logfileName) {
 
        //loop through the input string to find the first word
        int i = 0;
-       char currentChar;
 
        while(1){
         char currentChar = input[i];
-        if(currentChar == ' '){
-            
+        if(currentChar == ' ' || currentChar == '\0'){
+
             //put everything else in the Message array
             int k = 0;
             for (int j = i + 1; j < strlen(input); j++){
                 Message[k] = input[j];
                 k++;
             }
-            Message[k] = '\0'; 
+            Message[k] = '\0';
 
             //after that break
             break;
@@ -61,12 +59,14 @@ void logger(char *logfileName) {
        };
     Action[i] = '\0';
 
-    //now we will start writing to the log file 
-    char ActionString[105];
-    char MessageString[300];
-    snprintf(ActionString, sizeof(ActionString), "[%s]", Action);
-    snprintf(MessageString, sizeof(MessageString), " %s", Message);
+        //we will compare the strings lexiographically, quit without logging
+        if(strcmp(Action, quit) == 0){
+            break;
+        };
 
+    //now we will start writing to the log file
+    char ActionString[105];
+    snprintf(ActionString, sizeof(ActionString), "[%s]", Action);
 
     time_t now;
     struct tm *timeinfo;
@@ -74,26 +74,13 @@ void logger(char *logfileName) {
 
     time(&now);
     timeinfo = localtime(&now);
-    strftime(buffer, sizeof(buffer), "%Y-%m-%d %H:%M ", timeinfo);
+    strftime(buffer, sizeof(buffer), "%Y-%m-%d %H:%M", timeinfo);
 
-
-    fputs(buffer, file);
-    fputs(ActionString,file);
-    fputs(MessageString, file);
-    fputs("\n", file);
-        //we will compare the strings lexiographically
-        if(strcmp(Action, exit) == 0){
-            
-            //Always close the file at the end after write operations.
-                fclose(file);
-
-            break;
-            
-        };
+    fprintf(file, "%s %s %s\n", buffer, ActionString, Message);
     };
 
-
-
+    free(input);
+    fclose(file);
 
 
 }
